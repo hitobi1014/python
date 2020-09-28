13ef4fde140c8f6a4e2b7912cb658e1670931ad7
hitobi1014 <hitobi1014@gmail.com>
2020-09-28 18:21:47 +0900

    마우스 클릭 한 곳 좌표 구하기 전역변수

--- HelloPython/day05/omok01.py
+++ HelloPython/day05/omok01.py
@@ -4,12 +4,14 @@ from PyQt5 import uic
 from PyQt5.QtGui import *  # qPixmap을 사용하기 위해 임포트
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui, QtCore
+from spyder.app.tests.script import arr
 
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 form_class = uic.loadUiType("omok01.ui")[0]
-
+arr = [[0 for col in range(10)] for row in range(10)]
+msg ="33"
 #화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
@@ -19,18 +21,22 @@ class WindowClass(QMainWindow, form_class) :
         self.iw =QIcon("1.jpg")
         self.ib =QIcon("2.jpg")
         
-        
         for i in range(10):
             for j in range(10):
-                pb = QPushButton('', self)
-                pb.setGeometry(i*75,j*75,75,75)
-                pb.setIconSize(QtCore.QSize(75,75))
-                pb.setIcon(self.ie)
-                pb.clicked.connect(self.myclick)
-                
+                self.pb = QPushButton('', self)
+                self.pb.setIcon(self.ie)
+                self.pb.setGeometry(i*75,j*75,75,75)
+                self.pb.setIconSize(QtCore.QSize(75,75))
+                self.pb.setText(str(i)+","+str(j))
+                global arr
+                arr[i][j] = self.pb.text()
+#                 global msg
+#                 msg = "테스트"
+                self.pb.clicked.connect(self.myclick)
+#                 print(arr[i][j])
                 
     def myclick(self):
-        print("테스트")
+        print(arr[i][j])
         
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
