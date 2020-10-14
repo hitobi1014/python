79d6e035293b49499c5d48becc1f1018ef1d72b4
hitobi1014 <hitobi1014@gmail.com>
2020-10-14 17:17:07 +0900

    크롤링 naver 검색정보 DB에 넣기

--- HelloPython/day13/my_crawling06.py
+++ HelloPython/day13/my_crawling06.py
@@ -2,10 +2,11 @@ import os
 import sys
 import urllib.request
 from bs4 import BeautifulSoup
-import requests
+import sqlite3
+
 client_id = "Lu6xJQWzHIlKmx0vzb4T"
 client_secret = "Ryv55ePo9S"
-encText = urllib.parse.quote("대전 용문동 맛집")
+encText = urllib.parse.quote("대전 중구청 사쿠사쿠")
 # url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
 url = "https://openapi.naver.com/v1/search/local.xml?display=5&query=" + encText # xml 결과
 request = urllib.request.Request(url)
@@ -19,8 +20,33 @@ if(rescode==200):
 else:
     print("Error Code:" + rescode)
     
-# data = requests.get(url)
-# xml = data.text
-soup = BeautifulSoup(response_body,"html.parser")
+# soup = BeautifulSoup(response_body, 'html.parser')
+# my_items = soup.select(
+#     'item'
+#     )
+# 
+# conn = sqlite3.connect("naverSearchDB.db")
+# cur = conn.cursor()
+# for item in my_items:
+#     title = item.find('title').text
+#     link = item.find('link').text
+#     category = item.find('category').text
+#     description = item.find('description').text
+#     telephone= item.find('telephone').text
+#     
+#     address = item.find('address').text
+#     roadAddress = item.find('roadaddress').text
+#     telephone= item.find('telephone').text
+#     mapx = item.find('mapx').text
+#     mapy = item.find('mapy').text
+# #     print(roadAddress)
+#      
+#     cur.execute("INSERT INTO searchResult VALUES (?,?,?,?,?,?,?,?,?)",(title,link,category,description,telephone,address,roadAddress,mapx,mapy))
+#     
+# conn.commit()
+# cur.execute("select * from searchResult")
+# rows = cur.fetchall()
+# for row in rows:
+#     print(row)
+# conn.close()
 
-print(soup.find("item"))
--- HelloPython/day13/searchResult_insert.py
+++ HelloPython/day13/searchResult_insert.py
@@ -11,28 +11,28 @@ conn = sqlite3.connect("naverSearchDB.db")
 soup = BeautifulSoup(xml,"html.parser")
 
 print(soup.find_all("channel"))
-# for item in soup.find_all("item"):
-#     title = item.find("title").string
-#     link = item.find("link").string
-#     category = item.find("category").string
-#     description = item.find("description").string
-#     telephone = item.find("telephone").string
-#     address = item.find("address").string
-#     roadAddress = item.find("roadAddress").string
-#     mapx = item.find("mapx").string
-#     mapy = item.find("mapy").string
-#     
-#     cur = conn.cursor()
-# #     sql = "INSERT INTO searchResult VALUES (?,?,?,?,?,?,?,?,?)"
-# #     data2 = (title,link,category,description,telephone,address,roadAddress,mapx,mapy)
-# #     cur.execute(sql,data2)
-#     cur.execute("INSERT INTO searchResult VALUES (?,?,?,?,?,?,?,?,?)",(title,link,category,description,telephone,address,roadAddress,mapx,mapy))
-#     conn.commit()
-
-# cur = conn.cursor()
-# cur.execute("select * from searchResult")
-# rows = cur.fetchall()
-# for row in rows:
-#     print(row)
+for item in soup.find_all("item"):
+    title = item.find("title").string
+    link = item.find("link").string
+    category = item.find("category").string
+    description = item.find("description").string
+    telephone = item.find("telephone").string
+    address = item.find("address").string
+    roadAddress = item.find("roadAddress").string
+    mapx = item.find("mapx").string
+    mapy = item.find("mapy").string
+     
+    cur = conn.cursor()
+#     sql = "INSERT INTO searchResult VALUES (?,?,?,?,?,?,?,?,?)"
+#     data2 = (title,link,category,description,telephone,address,roadAddress,mapx,mapy)
+#     cur.execute(sql,data2)
+    cur.execute("INSERT INTO searchResult VALUES (?,?,?,?,?,?,?,?,?)",(title,link,category,description,telephone,address,roadAddress,mapx,mapy))
+    conn.commit()
+ 
+cur = conn.cursor()
+cur.execute("select * from searchResult")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
 conn.close()
 
